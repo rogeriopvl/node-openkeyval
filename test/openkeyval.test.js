bfc1f6cf8c4eeda239fdfc665ffd1150418792ad
rogeriopvl <rogeriopvl@gmail.com>
2012-08-04 01:24:02 +0100

    Improved error returns in callbacks.

--- test/openkeyval.test.js
+++ test/openkeyval.test.js
@@ -2,20 +2,47 @@ var OpenKeyVal = require('../lib/openkeyval');
 
 module.exports = {
 
-    testGetKeyWithoutSSL: function(test){
+    /**
+     * Test get of an existing key
+     */
+    testGetExistingKeyWithoutSSL: function(test){
         var okv = new OpenKeyVal(false);
-        okv.get('diogopms', function(err, res){
+        okv.get('rogeriopvl', function(err, res){
             test.ifError(err);
-            test.deepEqual(res, 'justtestingthisthing');
+            test.deepEqual(res, 'rogeriopvl.com');
+            test.done();
+        });
+    },
+
+    /**
+     * Test with key that does not exist (I hope so... :P)
+     */
+    testGetInexistentKeyWithoutSSL: function(test){
+        var okv = new OpenKeyVal(false);
+        okv.get('rogeriopvl_asdjkasdja', function(err, res){
+            test.ok(err);
+            test.ok(err.error);
+            test.done();
+        });
+    },
+
+    /**
+     * Test with invalid key (key with 3 chars is considered invalid by openkeyval
+     */
+    testGetInvalidKeyWithoutSSL: function(test){
+        var okv = new OpenKeyVal(false);
+        okv.get('lol', function(err, res){
+            test.ok(err);
+            test.ok(err.error);
             test.done();
         });
     },
 
     testGetKeyWithSSL: function(test){
         var okv = new OpenKeyVal(false);
-        okv.get('diogopms', function(err, res){
+        okv.get('rogeriopvl', function(err, res){
             test.ifError(err);
-            test.deepEqual(res, 'justtestingthisthing');
+            test.deepEqual(res, 'rogeriopvl.com');
             test.done();
         });
     },
